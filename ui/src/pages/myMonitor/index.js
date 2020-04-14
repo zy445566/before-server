563018741aa5dbe784977a76d5e38bb491452fb9
zy445566 <zyct@vip.qq.com>
2020-04-14 10:26:17 +0800

    基本功能完成

--- ui/src/pages/myMonitor/index.js
+++ ui/src/pages/myMonitor/index.js
@@ -43,15 +43,27 @@ export default class MyHome extends HTMLContent {
             return data.req.url.indexOf(this.fifterText)>-1;
         })
         const reqListDom = this.shadow.querySelector(".req-list");
-        reqListDom.innerHTML = ''
+        reqListDom.innerHTML = '';
         this.showReqList.map((data)=>{
             const reqItemTemplate = this.shadow.querySelector("#req-item-template");
             let reqItemTemplateContent = reqItemTemplate.content.cloneNode(true);
             const completeUrl = new URL(`${data.req.host}${data.req.url}`);
             reqItemTemplateContent.querySelector(".req-item").innerHTML = completeUrl.pathname;
+            reqItemTemplateContent.querySelector(".req-item").addEventListener('click',(event)=>{
+                for(const e of reqListDom.children) {e.className = e.className.replace('active','')}
+                if(event.target.className.indexOf('active')==-1) {
+                    event.target.className += ' active'
+                }
+                this.showBody(data);
+            })
             reqListDom.appendChild(reqItemTemplateContent)
         })
         // url innerhtml showReqList
+    }
+
+    showBody(data) {
+        const reqBody = this.shadow.querySelector(".req-body");
+        reqBody.innerHTML = JSON.stringify(data,null,2)
     }
 
     startSocket(config) {
