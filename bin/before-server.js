3c85f64aa76d29f95d22cc7d28c4e88775e0fe5a
zy445566 <zyct@vip.qq.com>
2020-04-15 15:25:39 +0800

    添加版本信息

--- bin/before-server.js
+++ bin/before-server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const proxyServerStart = require('../server/proxy');
 const monitorServerStart = require('../server/monitor');
+const package = require('../package.json');
 
+console.log(`version@${package.version}`)
 let koaEmitter;
 proxyServerStart((data)=>{
     koaEmitter.emit('proxy-request-info', data);
